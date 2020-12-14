19cc3990f09ce2280faab3cd9048dd555afd177a
Md. Shafi Kamal <bddp.shafi@gmail.com>
2020-12-14 15:54:53 +0600

    Add project files.

---
+++ simplewebservice/Controllers/date.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace simplewebservice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class date : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<date> _logger;
+
+        public date(ILogger<date> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            string dateTime = DateTime.Now.ToString("ddd dd MMM yyy HH:mm:ss BST yyy");
+            return Ok(new {date=dateTime, });
+
+
+        }
+    }
+}
